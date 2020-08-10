d23b3bbc728c4f82da0886f5cd877ec68573240a
xqr <1159641785@qq.com>
2020-08-10 13:23:29 +0800

    这是我的第一次提交

---
+++ 20200810/HelloWorld.java
@@ -0,0 +1,62 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+	}
+public static void main4(String[] args) {
+	
+	//作业2
+     int a=10;
+     int b=15;
+     int c=22;
+     int max=a;
+     max=a>b?a:b;
+     max=max>c?max:c;
+     System.out.println(max);
+}
+
+
+// 	//作业1
+// public static void main3(String[] args) {
+//     //1
+//     int a=3;
+// 	int b=5;
+// 	System.out.printf("a=%d  b=%d\n",a,b);
+//     int temp=a;
+//     a=b;
+//     b=temp;
+// 	System.out.printf("a=%d  b=%d\n",a,b);
+// 	// System.out.println("a=%d ,b=%d\n",a,b);error
+// 	// 
+// 	// 2
+// 	int a=3;
+// 	int b=5;
+// 	System.out.printf("a=%d  b=%d\n",a,b);
+//     int temp=a^b;
+//     a=temp^a;
+//     b=temp^b;
+// 	System.out.printf("a=%d  b=%d\n",a,b);
+
+ // System.out.println("please enter two numbers");
+// Scanner input = new Scanner(System.in);
+//}
+
+
+
+	public static void main2(String[] args) {
+		System.out.println("\\\"hello\\\"");
+	}
+
+
+	public static void main1(String[] args) {
+		System.out.println("HelloWorld");
+		int a=10;
+		//变量命名方式小驼峰
+		int aNub=20;
+		System.out.println(a);//换行输出
+		System.out.printf("%d\n",a);//格式化输出
+		System.out.print(a);//不换行
+		// System.out.print(aNub);
+		// System.out.println("HelloWorld");
+
+	}
+}
